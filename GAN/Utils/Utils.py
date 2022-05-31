e229ee90bffca8f2e13ed963543ca54c537f72d5
morfi <morfilo311@gmail.com>
2022-05-31 20:50:05 +0300

    GAN utils

--- GAN/Utils/Utils.py
+++ GAN/Utils/Utils.py
@@ -1,5 +1,11 @@
+import pandas as pd
+from bertopic import BERTopic
+from GAN_config import hparams
+
+
 def LoadAbstractPubMedData():
-    return pd.read_csv(config['data']['full'], encoding='utf8')
+    return pd.read_csv(hparams['PubMedData'], encoding='utf8')
+
 
 def LoadTopicModel():
-    return BERTopic.load(config['models']['topic_model_path'])
+    return BERTopic.load(hparams['topic_model_path'])
