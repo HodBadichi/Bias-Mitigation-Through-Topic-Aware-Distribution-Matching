Fixed GAN no gradients error on Sbert lossw

GAN/GANPubMed/src/PubMedGanSBert.py
@@ -8,7 +8,9 @@ import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
 from datetime import datetime
+from sentence_transformers import SentenceTransformer
 from transformers import AutoTokenizer, BertForMaskedLM, DataCollatorForLanguageModeling
+from train_sentence_bert import generate_sentence_similiariy_loss
 from sklearn.metrics import roc_auc_score, accuracy_score
 
 from GAN.Utils.src.TextUtils import BreakSentenceBatch
@@ -17,23 +19,21 @@ from GAN.Utils.src.TextUtils import BreakSentenceBatch
 """
 
 
-class PubMedGAN(pl.LightningModule):
+class PubMedGANSBert(pl.LightningModule):
     def __init__(self, hparams):
-        super(PubMedGAN, self).__init__()
+        super(PubMedGANSBert, self).__init__()
+        MODEL = 'all-MiniLM-L6-v2'
         self.hparams.update(hparams)
         self.bert_tokenizer = AutoTokenizer.from_pretrained(self.hparams['bert_tokenizer'])
         self.data_collator = DataCollatorForLanguageModeling(self.bert_tokenizer)
         self.max_length_bert_input = self.hparams['max_length_bert_input']
         self.max_sentences_per_abstract = self.hparams['max_sentences_per_abstract']
-        self.bert_model = BertForMaskedLM.from_pretrained(self.hparams['bert_pretrained_over_pubMed_path'])
-        # self.frozen_bert_model = BertForMaskedLM.from_pretrained(self.hparams['bert_pretrained_over_pubMed_path'])
-        self.sentence_embedding_size = self.bert_model.get_input_embeddings().embedding_dim
-        print("sentence_embedding_size", self.sentence_embedding_size)
-        # The linear layer if from 2 concat abstract (1 is bias and 1 unbiased) to binary label:
-        # 1 - the couple of matching docs was [biased,unbiased]
-        # 0 - the couple of matching docs was [unbiased,biased]
-        self.classifier = nn.Linear(self.sentence_embedding_size * self.max_sentences_per_abstract * 2, 1)
+        self.SentenceTransformerModel = SentenceTransformer(MODEL)
+        self.sentence_embedding_size = 384
         self.loss_func = torch.nn.BCEWithLogitsLoss(reduction='none')
+        self.classifier = nn.Linear(self.sentence_embedding_size * 3, 1) # *3 because of the number of inputs in the batch
+        self.save_model_path = os.path.join(self.hparams['SAVE_PATH'], f"Sbert_{MODEL}_{datetime.now(pytz.timezone('Asia/Jerusalem')).strftime('%y%m%d_%H%M%S.%f')}") 
+        os.makedirs(self.save_model_path, exist_ok=True)
 
     def forward(self):
         # Forward is unneeded , GaN model will not infer in the future
@@ -60,7 +60,7 @@ class PubMedGAN(pl.LightningModule):
                 step_ret_dict = {}
             step_ret_dict = self._generator_step(batch, step_ret_dict, name)
         return step_ret_dict
-
+    
     def training_step(self, batch: dict, batch_idx: int, optimizer_idx: int = None) -> dict:
         return self.step(batch, optimizer_idx, 'train_dataset')
 
@@ -85,39 +85,39 @@ class PubMedGAN(pl.LightningModule):
 
     def on_end(self, outputs, name):
         # outputs is a list (len=number of batches) of dicts (as returned from the step methods).
+        self._get_mean_from_outputs(outputs, name)
         if name == 'train_dataset' or name == 'test_dataset':
             outputs = outputs[0]  # TODO: WHY? only generator outputs. TODO: really?
         losses = torch.cat([output['losses'] for output in outputs])
         y_true = torch.cat([output['y_true'] for output in outputs])
         y_proba = torch.cat([output['y_proba'] for output in outputs])
         y_score = torch.cat([output['y_score'] for output in outputs])
-
-        self.log(f'debug/{name}_loss', losses.mean(), batch_size=self.hparams['batch_size'])
+        self.log(f'debug/{name}_loss', losses.mean(), batch_size=self.hparams['batch_size']) #TODO nofar and liel: understand what's the difference between this loss and the loss in the discriminator step - what are the graphs we expect to see?
         self.log(f'debug/{name}_accuracy', (1. * ((1. * (y_proba >= 0.5)) == y_true)).mean(),
                  batch_size=self.hparams['batch_size'])
         self.log(f'debug/{name}_1_accuracy', (1. * (y_proba[y_true == 1] >= 0.5)).mean(),
                  batch_size=self.hparams['batch_size'])
         self.log(f'debug/{name}_0_accuracy', (1. * (y_proba[y_true == 0] < 0.5)).mean(),
                  batch_size=self.hparams['batch_size'])
-
         if name == 'val_dataset':
-            # save model at the end of val_dataset epoch
-            if self.hparams['max_epochs'] > 0:
-                dir_path = os.path.join(self.hparams['SAVE_PATH'],
-                                        f"bert_GAN_model_{datetime.now(pytz.timezone('Asia/Jerusalem')).strftime('%y%m%d_%H%M%S.%f')}")
-                self.bert_model.save_pretrained(os.path.join(dir_path, f'epoch{self.hparams["max_epochs"] - 1}'))
-                if not os.path.exists(f'{dir_path}/config'):
-                    # save the config to all of this GAN workflow, will be saved on first epoch
-                    np.save(f'{dir_path}/config', self.hparams)
+            path = os.path.join(self.save_model_path, f"epoch_{self.current_epoch}")
+            if self.current_epoch > 0  and not os.path.exists(path):
+                os.makedirs(path)
+                self.SentenceTransformerModel.save(path)
+                if not os.path.exists(f'{path}/config'):
+                    np.save(f'{path}/config', self.hparams)
+
 
     def configure_optimizers(self):
         # Discriminator step paramteres -  classifier.
         grouped_parameters_discriminator = [{'params': self.classifier.parameters()}]
         optimizer_discriminator = torch.optim.Adam(grouped_parameters_discriminator, lr=self.hparams['learning_rate'])
         # Generator step parameters - only 'the bert model.
-        grouped_parameters_generator = [{'params': self.bert_model.parameters()}]
+        # grouped_parameters_generator = [{'params': self.bert_model.parameters()}]
+        grouped_parameters_generator = [{'params': self.SentenceTransformerModel.parameters()}]
         optimizer_generator = torch.optim.Adam(grouped_parameters_generator, lr=self.hparams['learning_rate'])
         return [optimizer_discriminator, optimizer_generator]
+
 
     """################# DISCRIMINATOR FUNCTIONS #####################"""
 
@@ -132,7 +132,7 @@ class PubMedGAN(pl.LightningModule):
         since not all batch items represent a couple of docs to discriminator (some didn't get match with noahArc matcher)
         we clean (leave) the relevant docs in the batch, shuffle them, get prediction and return loss
         """
-        result_dictionary = {'mlm_loss': 0, 'optimizer_idx': 0}
+        result_dictionary = {'sentence_similiarity_loss': 0, 'optimizer_idx': 0}
         # {'loss': , 'losses': , 'mlm_loss': , 'y_true': , 'y_proba': , 'y_score': , 'optimizer_idx': }
         clean_discriminator_batch = self._discriminator_clean_batch(batch)
         if len(clean_discriminator_batch) == 0:
@@ -153,10 +153,13 @@ class PubMedGAN(pl.LightningModule):
                 1 - discriminator_y_true.cpu().detach()) * (1 - y_proba)
         if not all(discriminator_y_true) and any(discriminator_y_true):
             # Calc auc only if batch has more than one class.
-            self.log(f'discriminator/{name}_auc', roc_auc_score(discriminator_y_true.cpu().detach(), y_proba),
-                     batch_size=self.hparams['batch_size'])
-        self.log(f'discriminator/{name}_accuracy', accuracy_score(discriminator_y_true.cpu().detach(), y_proba.round()),
-                 batch_size=self.hparams['batch_size'])
+            auc = roc_auc_score(discriminator_y_true.cpu().detach(), y_proba)
+            result_dictionary['auc'] = auc
+            self.log(f'discriminator/{name}_auc', auc, batch_size=self.hparams['batch_size'])
+        accuracy = accuracy_score(discriminator_y_true.cpu().detach(), y_proba.round())
+        result_dictionary['accuracy'] = accuracy
+        self.log(f'discriminator/{name}_accuracy', accuracy, batch_size=self.hparams['batch_size'])
+
         return result_dictionary
 
     def _discriminator_clean_batch(self, batch):
@@ -179,44 +182,23 @@ class PubMedGAN(pl.LightningModule):
                 result_batch.append(unbiased_text)
                 result_batch.append(biased_text)
         return result_batch
-
-    def _discriminator_get_cls_bert_outputs(self, bert_inputs):
-        all_outputs = self.bert_model(**bert_inputs, output_hidden_states=True).hidden_states[-1]
-        cls_outputs = all_outputs[:, 0]
-        return cls_outputs
-
-    def _discriminator_bert_embeddings_to_predictions(self, bert_cls_outputs, begin_end_indexes):
+    
+    def _discriminator_SBERT_embeddings_to_predictions(self, sentence_embeddings):
         sample_embedding = []
 
-        def fix_sentences_size(sent_embeddings):
-            """
-            Fix embedding size in case it requires padding or truncating
-
-            :param sent_embeddings:
-            :return:
-            """
-            if len(sent_embeddings) > self.max_sentences_per_abstract:  #
-                # Too many sentences 
-                truncated_embeddings = sent_embeddings[:self.max_sentences_per_abstract]
-                return torch.flatten(truncated_embeddings)
-            else:
-                padding = torch.zeros(self.max_sentences_per_abstract - len(sent_embeddings),
-                                      self.sentence_embedding_size,
-                                      device=self.device)
-                return torch.flatten(torch.cat([sent_embeddings, padding], dim=0))
-
-        for i in range(0, len(begin_end_indexes), 2):
-            start_index_first_document, end_index_first_document = begin_end_indexes[i]
-            start_index_second_document, end_index_second_document = begin_end_indexes[i + 1]
-            first_document_embeddings = fix_sentences_size(
-                bert_cls_outputs[start_index_first_document: end_index_first_document])
-            second_document_embeddings = fix_sentences_size(
-                bert_cls_outputs[start_index_second_document: end_index_second_document])
-            curr_concat_embeddings = torch.cat((first_document_embeddings, second_document_embeddings))
+        for i in range(0, len(sentence_embeddings), 2):
+            first_document_embeddings = sentence_embeddings[i]
+            second_document_embeddings = sentence_embeddings[i + 1]
+            curr_concat_embeddings = torch.cat((
+                first_document_embeddings,
+                second_document_embeddings,
+                abs(first_document_embeddings - second_document_embeddings))
+            )
             sample_embedding.append(curr_concat_embeddings)
 
         aggregated = torch.stack(sample_embedding)
         y_predictions = self.classifier(aggregated).squeeze(1)
+        # print(f"prediction is {len(y_predictions)}")
         return y_predictions
 
     def _discriminator_get_predictions(self, batch, shuffle_vector):
@@ -231,13 +213,9 @@ class PubMedGAN(pl.LightningModule):
         """
 
         discriminator_batch = self._discriminator_get_batch(batch, shuffle_vector)
-
-        begin_end_indexes, documents_sentences, max_len = BreakSentenceBatch(discriminator_batch)
-
-        bert_inputs = self._get_bert_inputs(documents_sentences)
-
-        bert_cls_outputs = self._discriminator_get_cls_bert_outputs(bert_inputs)
-        return self._discriminator_bert_embeddings_to_predictions(bert_cls_outputs, begin_end_indexes)
+        sentence_embeddings = self.SentenceTransformerModel.encode(discriminator_batch, convert_to_tensor=True)
+        # print(f"embeddings lengths is {len(sentence_embeddings[0])}")
+        return self._discriminator_SBERT_embeddings_to_predictions(sentence_embeddings)
 
     """################# GENERATOR FUNCTIONS #####################"""
 
@@ -249,30 +227,16 @@ class PubMedGAN(pl.LightningModule):
         else:
             discriminator_loss = discriminator_step_ret_dict['loss']
         step_ret_dict['optimizer_idx'] = 1
-        # {'loss': , 'losses': , 'mlm_loss': , 'y_true': , 'y_proba': , 'y_score': , 'optimizer_idx': }
-        generator_batch = self._generator_get_batch(batch)
-        begin_end_indexes, documents_sentences, max_len = BreakSentenceBatch(generator_batch)
-        bert_inputs = self._get_bert_inputs(documents_sentences)
-        mlm_loss = self._generator_get_mlm_loss(bert_inputs)
-        print(f"MLM loss is {mlm_loss}")
-        step_ret_dict['mlm_loss'] = mlm_loss
+        # {'loss': , 'losses': , 'sentence_similiarity_loss': , 'y_true': , 'y_proba': , 'y_score': , 'optimizer_idx': }
+        sentence_similarity_loss = generate_sentence_similiariy_loss(batch, self.SentenceTransformerModel)
+        step_ret_dict['sentence_similiarity_loss'] = sentence_similarity_loss
         # TODO diff from frozen and tune the factors (mlm_loss is 2-5, discriminator_loss is ~0.5-1)
-        total_loss = self.hparams['mlm_factor'] * mlm_loss - self.hparams['discriminator_factor'] * discriminator_loss
+        total_loss = self.hparams['sentence_similiarity_factor'] * sentence_similarity_loss - self.hparams['discriminator_factor'] * discriminator_loss
         step_ret_dict['loss'] = total_loss
         self.log(f'generator/{name}_loss', total_loss, batch_size=self.hparams['batch_size'])
-        self.log(f'generator/{name}_mlm_loss', mlm_loss, batch_size=self.hparams['batch_size'])
+        self.log(f'generator/{name}_sent_sim_loss', sentence_similarity_loss, batch_size=self.hparams['batch_size'])
         self.log(f'generator/{name}_discriminator_loss', discriminator_loss, batch_size=self.hparams['batch_size'])
         return step_ret_dict
-
-    def _generator_get_mlm_loss(self, inputs):
-        """returns MLM loss"""
-        collated_inputs = self.data_collator(inputs['input_ids'].tolist())
-        collated_inputs = {k: v.to(self.device) for k, v in collated_inputs.items()}
-
-        inputs['input_ids'] = collated_inputs['input_ids']
-        inputs['labels'] = collated_inputs['labels']
-        loss = self.bert_model(**inputs).loss
-        return loss
 
     def _generator_get_batch(self, batch):
         result_batch = []
@@ -281,17 +245,25 @@ class PubMedGAN(pl.LightningModule):
         return result_batch
 
     """################# UTILS FUNCTIONS #####################"""
-    
 
-    def _get_bert_inputs(self, documents_sentences):
-        inputs = self.bert_tokenizer.batch_encode_plus(documents_sentences, padding=True, truncation=True,
-                                                       max_length=self.max_length_bert_input,
-                                                       add_special_tokens=True, return_tensors="pt")
-        inputs = {k: v.to(self.device) for k, v in inputs.items()}
-        return inputs
+    def _get_mean_from_outputs(self, outputs, name):
+        """
+        :param outputs: list of dictionaries from epoch
+        :param name: name is either "test", "train" or "val"
+        This function will log to wandb the mean $name accuracy of the epoch
+        """
+        accuracy_and_auc_results = {}
+        accuracy_and_auc_results['accuracy'] = [output['accuracy'] for output in outputs if 'accuracy' in output]
+        accuracy_and_auc_results['auc'] = [output['auc'] for output in outputs if 'auc' in output]
+        accuracy_mean = np.mean(accuracy_and_auc_results['accuracy'])
+        auc_mean = np.mean(accuracy_and_auc_results['auc'])
+        self.log(f'discriminator/{name}_accuracy_score_per_epoch', accuracy_mean, on_epoch=True, prog_bar=True)
+        self.log(f'discriminator/{name}_auc_score_per_epoch', auc_mean, on_epoch=True, prog_bar=True)
 
     def _convert_to_list_of_dicts(self, batch):
         # to make it a shape of {'origin':int,'biased':string,'unbiased':string}
         batch_df = pd.DataFrame.from_dict(batch)
         batch = batch_df.T.to_dict().values()
         return batch
+
+